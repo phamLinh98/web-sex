9a473249b8fb6c361ecfea42ef9e1c813e0cb227
PhamTuanLinh <58585884+phamLinh98@users.noreply.github.com>
2024-01-28 21:14:37 +0900

    linh commit useContext (#4)
    * linh demo cmt
    
    * fix router
    
    * fix bug route
    
    * refactoring code
    
    * linh fix config code refactoring
    
    * linh commit useContext

--- src/uploadPicture/uploadPicture.jsx
+++ src/uploadPicture/uploadPicture.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { createContext } from "react";
+import Demo1 from "./Demo1";
+import Demo2 from "./Demo2";
+import Demo3 from "./Demo3";
 
+export const Context = createContext();
 export default function UploadPicture() {
-  return <div>Chuc nang upload Picture</div>;
+  return (
+    <Context.Provider value={{ data: "Cap phat du lieu" }}>
+      <Demo1 />
+      <Demo2 />
+      <Demo3 />
+    </Context.Provider>
+  );
 }
