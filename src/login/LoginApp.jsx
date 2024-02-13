e41f36639b6e4d5ec0515c1d89bb5d9a16c39972
phamLinh98 <linhthusinh98@gmail.com>
2024-02-13 21:23:55 +0900

    update login sso

--- src/login/LoginApp.jsx
+++ src/login/LoginApp.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import { useAuth } from "../layouts/AuthenticationLayout";
-import { Navigate } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 export default function LoginApp() {
-  const { user } = useAuth();
-  if (user) {
-    return <Navigate to="/" />;
-  }
+  const auth = getAuth();
+  const handleLoginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const {
+      user: { displayName, email, photoURL, uid },
+    } = await signInWithPopup(auth, provider);
+    const user = auth.currentUser;
+    console.log(user);
+    if (user) {
+      console.log(user);
+    }
+  };
+  useEffect(() => {}, []);
   return (
     <div>
       <h1 className="">Login</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="button"
+        onClick={handleLoginWithGoogle}
+      >
         Login with Google
       </button>
     </div>
