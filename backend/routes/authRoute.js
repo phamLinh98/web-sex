58b1d76a0f598be0972dcdba3d6f5ed864502eb4
phamLinh98 <linhthusinh98@gmail.com>
2024-02-18 23:14:09 +0900

    update login authen

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Schema, model } from "mongoose";
 import jwt from "jsonwebtoken";
 import { envConfig } from "../configs/envConfig.js";
+import bcrypt from "bcryptjs";
 const authRoute = Router();
 
 const userSchema = new Schema({
@@ -23,16 +24,16 @@ authRoute.post("/register", async (req, res) => {
   if (user) {
     return res.send("User already exists");
   }
-  const hashCode = {};
-  const newUser = new User({ email, password });
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ email, password: hashPassword });
   await newUser.save();
   res.send("User created");
 });
 
-authRoute.get("/login", async (req, res) => {
+authRoute.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-  console.log("user", user);
+  const hashPassword = await bcrypt.hash(password, 10);
+  const user = await User.findOne({ email, password: hashPassword });
   if (user) {
     const token = jwt.sign({ id: user._id, email: user }, envConfig.JWT_SECRET);
     console.log("token", token);
