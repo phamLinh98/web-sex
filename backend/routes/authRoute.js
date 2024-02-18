c62db940159d3a60c4b73637261418b94aa47058
“linhPham98” <linhthusinh98@gmail>
2024-02-18 23:53:03 +0900

    update docker

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -18,17 +18,17 @@ const userSchema = new Schema({
 
 const User = model("User", userSchema);
 authRoute.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  // eslint-disable-next-line no-undef
-  const user = await User.findOne({ email });
-  if (user) {
-    return res.send("User already exists");
-  }
-  const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ email, password: hashPassword });
-  await newUser.save();
-  res.send("User created");
-});
+      const { email, password } = req.body;
+// eslint-disable-next-line no-undef
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.send("User already exists");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashPassword });
+    await newUser.save();
+    res.send("User created");
+  });
 
 authRoute.post("/login", async (req, res) => {
   const { email, password } = req.body;
