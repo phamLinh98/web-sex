35c621f441d00fab6f140114df1cd3282e5a613b
phamLinh98 <linhthusinh98@gmail.com>
2024-02-18 21:53:35 +0900

    update login authenication

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import authRoute from "./authRoute.js";
 import apiRoute from "./apiRoute.js";
+import { authenticated } from "../middleware/authenticated.js";
 
 const allRoutes = Router();
+
 allRoutes.use(authRoute);
-allRoutes.use("/api", apiRoute);
+
+allRoutes.use("/api", authenticated, apiRoute);
+
 export default allRoutes;
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,13 +1,34 @@
 import { Router } from "express";
-
+import { Schema, model } from "mongoose";
 const authRoute = Router();
 
-authRoute.get("/login", (req, res) => {
-  res.send("Login Route");
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-authRoute.get("/register", (req, res) => {
+const User = model("User", userSchema);
+authRoute.get("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ email });
   res.send("Register Route");
+  if (user) {
+    return res.send("User already exists");
+  }
+  const newUser = new User({
+    username,
+    password,
+  });
+  await newUser.save();
+  res.send("Register route");
 });
+
+authRoute.get("/login", async (req, res) => {});
 
 export default authRoute;
