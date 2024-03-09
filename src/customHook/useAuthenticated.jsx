cdf6bf8af0166ad26985b996621cc04d5dd1572a
“linhPham98” <linhthusinh98@gmail>
2024-03-10 02:19:05 +0900

    last update login password

--- src/customHook/useAuthenticated.jsx
+++ src/customHook/useAuthenticated.jsx
@@ -1,10 +1,30 @@
-// import React from "react";
-// import { useContext } from "react";
-// import {loginUserContext} from
-// export default function useAuthenticated() {
-//   const { loginUser } = useContext(loginUserContext);
-//   if (loginUser) {
-//     return loginUser;
-//   }
-//   return null;
-// }
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getUserInfo } from "../services/AuthServices";
+
+export function useAuthenticated(setIsLoading) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsLoading(true);
+    getUserInfo()
+      .then((data) => {
+        if (data.loginUser.id) {
+          //setLoginUser(data.loginUser);
+        } else {
+          navigate("/login");
+        }
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        navigate("/login");
+      });
+
+    return () => {
+      setIsLoading(false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return {};
+}
--- src/layouts/AuthenticationLayout.jsx
+++ src/layouts/AuthenticationLayout.jsx
@@ -1,35 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import { getUserInfo } from "../services/AuthServices";
-//import Loading from "../components/Loading";
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useAuthenticated } from "../customHook/useAuthenticated";
 
 export default function AuthenticationLayout() {
   const [isLoading, setIsLoading] = useState(false);
-  //const { loginUser, setLoginUser } = useContext(LoginUserContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setIsLoading(true);
-    getUserInfo()
-      .then((data) => {
-        if (data.loginUser.id) {
-          //setLoginUser(data.loginUser);
-        } else {
-         // setLoginUser({});
-          navigate("/login");
-        }
-        setIsLoading(false);
-      })
-      .catch(() => {
-        setIsLoading(false);
-        navigate("/login");
-      });
-
-    return () => {
-      setIsLoading(false);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  useAuthenticated(setIsLoading);
   if (!isLoading) {
     return (
       <div>
